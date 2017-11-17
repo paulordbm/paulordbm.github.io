71ff1c3ab1a6909c03b5d4ccabd129cc71ea385e
Paulo Mendonça <paulo.mendonca@indt.org.br>
2017-11-17 09:37:50 -0400

    Added tailwind css configuration.

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ type Props = {};
 class App extends Component<Props> {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="w-screen h-screen flex flex-col justify-center align-center bg-grey-darker">
+        <div className="mx-auto max-w-sm rounded overflow-hidden shadow-lg bg-grey-lighter">
+          <img src={logo} className="w-full mx-auto h-24 app-logo" alt="logo" />
+          <div className="px-6 px-4 mb-6">
+            <div className="font-bold text-xl mb-2">Welcome to React</div>
+            <p className="text-grey-darker text-base">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
